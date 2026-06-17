092bbdd0022fd74d4cd6be2a95dd37157254ecbe
Brahiyan <133051208+Brahiyan@users.noreply.github.com>
2026-06-17 10:24:45 -0300

    cambio a contador tiempo y hitboxes

--- UI/contador_tiempo.gd
+++ UI/contador_tiempo.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var tiempo_label: RichTextLabel = $Panel/RichTextLabel
+@onready var tiempo_label: RichTextLabel = $RichTextLabel
 
 var tiempo: float = 0.0
 
@@ -12,10 +12,10 @@ func actualizar_tiempo(tiempo: float) -> void:
 	
 	tiempo_label.text = (
 	"[center][wave amp=10 freq=2]" +
-	"[color=#00FFAA]%02d[/color]" % minutos +
-	"[color=#00FFAA]:[/color]" +
-	"[color=#00FFAA]%02d[/color]" % segundos +
-	"[color=#00FFAA]:[/color]" +
-	"[color=#00FFAA]%02d[/color]" % milisegundos +
+	"[color=#FF0015]%02d[/color]" % minutos +
+	"[color=#FF0015]:[/color]" +
+	"[color=#FF0015]%02d[/color]" % segundos +
+	"[color=#FF0015]:[/color]" +
+	"[color=#FF0015]%02d[/color]" % milisegundos +
 	"[/wave][/center]"
 )
